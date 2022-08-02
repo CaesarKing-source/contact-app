0db0ceba98fa4f5100978ce028589e18e7dcb31e
Akshay Giri <65717442+CaesarKing-source@users.noreply.github.com>
2022-08-02 12:43:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import Contact from '.componentes/Contact';
+import AddContact from '.componentes/AddContact';
+import ContactCard from './components/ContactCard';
+
+
+function App() {
+  return (
+   <div>
+    Hello World
+   </div>
+  );
+}
+
+export default App;
